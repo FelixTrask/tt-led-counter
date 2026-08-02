2d08617368fdde0738a7f6e0fe66aed3319ec4d6
Felix Trask <felixtrask@gmail.com>
2026-08-02 15:21:26 -0400

    Fix clock unit syntax and add FallingEdge signal checks

--- test/test.py
+++ test/test.py
@@ -3,7 +3,7 @@
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, FallingEdge
 
 
 @cocotb.test()
@@ -11,7 +11,7 @@ async def test_project(dut):
     dut._log.info("Starting LED Counter Test!")
 
     # Set the clock period to 10MHz
-    clock = Clock(dut.clk, 100, unit="ns")
+    clock = Clock(dut.clk, 100, units="ns")
     cocotb.start_soon(clock.start())
 
     # Reset
@@ -21,16 +21,21 @@ async def test_project(dut):
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
+
+    await FallingEdge(dut.clk)
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
 
+    await FallingEdge(dut.clk)
     assert dut.uo_out.value == 0, f"Expected 0 after reset, got {dut.uo_out.value}"
 
     await ClockCycles(dut.clk, 65536) # Bit 0 changes every 65536 cycles (2^16)
+    await FallingEdge(dut.clk)
     assert dut.uo_out.value == 1, f"Expected 1, got {dut.uo_out.value}"
 
     await ClockCycles(dut.clk, 65536)
+    await FallingEdge(dut.clk)
     assert dut.uo_out.value == 2, f"Expected 2, got {dut.uo_out.value}"
 
     dut._log.info("LED Counter test passed successfully!")
