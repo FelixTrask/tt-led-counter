9e77fc61ed94e6f31a1f28da040173a4d4540b36
Felix Trask <felixtrask@gmail.com>
2026-08-02 15:04:21 -0400

    Fix module name mismatch to tt_um_felixtrask

--- src/project.v
+++ src/project.v
@@ -1,11 +1,11 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Felix Trask
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_felixtrask (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,10 +16,22 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  reg [23:0] count; // Save clock ticks, number from 0 to 2^23
+
+  always @(posedge clk or negedge rst_n) begin // Begin if the clock is high or the reset pin is low
+    if (!rst_n) begin // If the reset pin is low:
+      count <= 24'b0; // Reset the count to 0 (24'b0 = 24 bytes, set to a 0)
+    end else if (ena) begin // If ena is high (should always be):
+      count <= count + 1'b1; // Increment the count by 1 (1'b1 = 1 byte, set to a 1)
+    end
+  end
+
+  // Assign dedicated output to the top 8 bits of our count register
+  assign uo_out = count[23:16];
+
+  // Assign unused outputs
+  assign uio_out = 8'b0;
+  assign uio_oe  = 8'b0;
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
